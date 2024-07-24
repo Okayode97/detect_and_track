1afe37007472420726a732f36501cdc971ac13d1
Kayode Sonaike <Okayode97@gmail.com>
2024-07-24 22:04:21 +0100

    WIP

--- src/detector.py
+++ src/detector.py
@@ -1,16 +1,54 @@
 """
 Script to run detection on an image and return bounding boxes in format (xyhw) such that it can then be tracked by tracker.
 """
-from torchvision.models.detection import ssdlite320_mobilenet_v3_large, SSDLite320_MobileNet_V3_Large_Weights
 import torch
+from torchvision.models.detection import ssdlite320_mobilenet_v3_large
+from torchvision.models.detection import SSDLite320_MobileNet_V3_Large_Weights
 import time
+import cv2
+import numpy as np
+import logging
 
-model = ssdlite320_mobilenet_v3_large(weights=SSDLite320_MobileNet_V3_Large_Weights.DEFAULT)
-model.eval()
-x = [torch.rand(3, 320, 320), torch.rand(3, 500, 400)]
 
-start_time = time.time()
-predictions = model(x)
-print("FPS: ", 1.0 / (time.time() - start_time))
+list_of_captured_image = []
+ssd_model = ssdlite320_mobilenet_v3_large(weights=SSDLite320_MobileNet_V3_Large_Weights.DEFAULT)
+ssd_model.eval()
 
-print(f"{predictions=}")
+def run_object_detection(frame: torch.tensor):
+    predictions = ssd_model(frame)
+    print(f"Prediction: {predictions}")
+    pass
+
+def capture_image():
+    CAM = cv2.VideoCapture(0)
+
+    if CAM.isOpened():
+        print("Camera is opened")
+    else:
+        print("Unable to open camera")
+
+    while CAM.isOpened():
+        ret, frame = CAM.read()
+
+        if not ret:
+            print("Unable to read frame from camera...")
+            break
+
+        cv2.imshow("Image", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            # list_of_captured_image.append(frame)
+            print("Captured image...")
+            color_first = np.moveaxis(frame, -1, 0)
+            color_first = np.expand_dims(color_first, 0)
+            print(color_first.shape)
+            torch_frame = torch.from_numpy(color_first)
+            print(torch_frame.shape)
+            run_object_detection(torch_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+if __name__ == "__main__":
+    capture_image()
