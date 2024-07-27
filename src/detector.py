1a446005facee97a631d444264677cb16e57ec86
Kayode Sonaike <Okayode97@gmail.com>
2024-07-27 12:22:53 +0100

    - Added labels on model output - minor clean up and refactors - Added todos for further tasks

--- src/detector.py
+++ src/detector.py
@@ -1,14 +1,19 @@
 """
 Script to run detection on an image and return bounding boxes in format (xyhw) such that it can then be tracked by tracker.
+possible TODO:
+- Consideration on improving fps (model optimization, use of more efficient models, portable hardware accelerator??)
+- further roboust tracking: DeepSORT, multi-object tracking to track multiple objects
+- Implement interface to allow for dynamic target selection
 """
 import torch
 from torchvision.models.detection import ssdlite320_mobilenet_v3_large
 from torchvision.models.detection import SSDLite320_MobileNet_V3_Large_Weights
-from torchvision import transforms
+from torchvision.ops import box_convert
 import time
 import cv2
 import numpy as np
 import logging
+from label import coco_labels
 
 
 IMAGE_DIM = 320
@@ -17,8 +22,9 @@ IMAGE_DIM = 320
 ssd_model = ssdlite320_mobilenet_v3_large(weights=SSDLite320_MobileNet_V3_Large_Weights.DEFAULT)
 ssd_model.eval()
 
+
 # frame: batched or single images (B, C, H, W) with images rescaled to 0.0, 1.0
-def run_object_detection(frame: np.ndarray):
+def run_object_detection(frame: np.ndarray) -> dict[torch.Tensor]:
     # transpose the image to color first
     # do the image need to be resized to specific height & width?
     frame = frame.transpose((2, 0, 1))
@@ -31,14 +37,12 @@ def run_object_detection(frame: np.ndarray):
     print(f"============== FPS: {1.0 /(time.time() - start_time)} ==============")
 
     # dict with keys: box, scores, label
-    # box format?? xyxy
+    # single batch
     return predictions[0]
 
-
-preprocess = transforms.Compose([
-    transforms.ToTensor(),
-    transforms
-])
+# returned box in format of xyxy, tracker requires box in format xyhw
+def convert_box_format(box_xyxy: np.ndarray) -> np.array:
+    return box_convert(torch.from_numpy(box_xyxy), "xyxy", "xywh").detach().numpy()
 
 def run_inference_on_live_image():
 
@@ -66,18 +70,19 @@ def run_inference_on_live_image():
 
         # get the top 3 prediction
         top_three_detection_scores: np.array = np.sort(scores, axis=0)[-3:]
-        top_three_bbox = []
-        top_three_labels = []
+        top_three_bbox: list[int] = []
+        top_three_labels: list[int] = []
 
         for score in top_three_detection_scores:
             index = np.where(scores == score)[0][0]
             top_three_bbox.append(bbox[index])
             top_three_labels.append(labels[index])
 
-        for detection in top_three_bbox:
-            print(detection)
+        for detection, label_index in zip(top_three_bbox, top_three_labels):
             cv2.rectangle(frame, (int(detection[0]), int(detection[1])),
                                  (int(detection[2]), int(detection[3])), (0, 255, 0), 1)
+            cv2.putText(frame, coco_labels[label_index], (int(detection[0])+50, int(detection[1])+50), cv2.FONT_HERSHEY_SIMPLEX,
+                         1, (0, 255, 0), 2, cv2.LINE_AA) 
 
         cv2.imshow("Image", frame)
         if cv2.waitKey(1) == ord('q'):
