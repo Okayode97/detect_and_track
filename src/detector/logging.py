8c8ab343b0656034778c7b862fd20507fa5e9537
Kayode Sonaike <Okayode97@gmail.com>
2024-10-14 22:31:38 +0100

    Added implementation and test for log_results function and incremental average function

--- src/detector/logging.py
+++ src/detector/logging.py
@@ -1,4 +1,40 @@
 from typing import Optional
+import os
+import json
 
-def log_results(data: dict, filename: str, model_name:Optional[str] = None):
-    pass
+def incremental_average(current_average: float, new_value: float, count: int) -> float:
+    # https://math.stackexchange.com/questions/106700/incremental-averaging
+    if count > 2:
+        return current_average + ((new_value - current_average)/count)
+    
+    # calculate average if less than two entries
+    return sum([current_average, new_value]) / (count + 1)
+
+def log_results(data: dict, filename: str, model_name: str):
+
+    # if the file exists load the data, else keep empty dict
+    loaded_data = {}
+    if os.path.exists(f"{filename}_log.json"):
+        with open(f"{filename}_log.json", "r") as f:
+            loaded_data = json.load(f)
+    
+    # check if model_name exist in loaded data
+    # if it does not creates a new dict to store the data
+    if model_name not in loaded_data.keys():
+        loaded_data[model_name] = {}
+    
+    # update existing dict with values stored in data input
+    for metric in data.keys():
+        if metric not in loaded_data[model_name].keys():
+            loaded_data[model_name][metric] = {"avg": data[metric], "count": 1}
+
+        else:
+            loaded_data[model_name][metric]["avg"] = incremental_average(loaded_data[model_name][metric]["avg"],
+                                                                         data[metric],
+                                                                         loaded_data[model_name][metric]["count"])
+            loaded_data[model_name][metric]["count"] += 1
+
+    
+    # write loaded_data to file
+    with open(f"{filename}_log.json", "w") as f:
+        json.dump(loaded_data, f, indent=4)
--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,9 +1,77 @@
 import os
-from src.detector.logging import log_results
+import json
+import pytest
+from src.detector.logging import log_results, incremental_average
 
 
-def test_if_log_file_does_not_exist_file_is_created():
-    data = {"metrics": {"elasped_time": 60.0, "fps": 20.0}}
-    assert not os.path.exists("test_log.json")
-    log_results(data, "test")
-    assert os.path.exists("test_log.json")
+class TestLogResults:
+    def test_if_log_file_does_not_exist_file_is_created(self):
+        data = {"elasped_time": 60.0, "fps": 0.017}
+        assert not os.path.exists("test_log.json")
+        log_results(data, "test", "test_model")
+        assert os.path.exists("test_log.json")
+        os.unlink("test_log.json")
+
+    def test_new_logged_data_is_appended_to_created_file(self):
+        data = {"elasped_time": 30.0, "fps": 0.03}
+        log_results(data, "test", "test_model")
+        
+        with open("test_log.json", "r") as f:
+            logged_data = json.load(f)
+        
+        expected_logged_data = {"test_model": {"elasped_time": {"avg": 30.0, "count": 1}, "fps": {"avg": 0.03, "count": 1}}}
+        assert expected_logged_data == logged_data    
+        os.unlink("test_log.json")
+
+    def test_logged_metrics_are_aggregated_with_multiple_calls(self):
+        data = {"elasped_time": 60.0, "fps": 0.017}
+        log_results(data, "test", "test_model")
+
+        data = {"elasped_time": 65.0, "fps": 0.015}
+        log_results(data, "test", "test_model")
+
+        data = {"elasped_time": 10.0, "fps": 0.1}
+        log_results(data, "test", "test_model_2")
+        
+        with open("test_log.json", "r") as f:
+            logged_data = json.load(f)
+        
+        expected_logged_data = {"test_model": {"elasped_time": {"avg": 62.5, "count": 2},
+                                               "fps": {"avg": 0.016, "count": 2}},
+                                "test_model_2": {"elasped_time": {"avg": 10.0, "count": 1},
+                                                 "fps": {"avg": 0.1, "count": 1}}
+                                }
+
+        assert expected_logged_data == logged_data
+        os.unlink("test_log.json")
+
+    def test_new_metrics_can_be_added_after_several_calls(self):
+        data = {"elasped_time": 60.0, "fps": 0.017}
+        log_results(data, "test", "test_model")
+
+        data = {"elasped_time": 65.0, "fps": 0.015, "cpu_useage": 2.0}
+        log_results(data, "test", "test_model")
+
+        with open("test_log.json", "r") as f:
+            logged_data = json.load(f)
+        
+        expected_logged_data = {"test_model": {"elasped_time": {"avg": 62.5, "count": 2},
+                                            "fps": {"avg": 0.016, "count": 2},
+                                            "cpu_useage": {"avg": 2.0, "count": 1}}
+                                }
+
+        assert expected_logged_data == logged_data
+        os.unlink("test_log.json")
+
+
+class TestIncrementalAverage:
+    @pytest.mark.parametrize("data, new_val", (([0, 1, 1, 2, 3, 5, 8], 13),
+                                               ([1, 2, 3, 4], 5)))
+    def test_average_correctly_recalculated(self, data, new_val):
+        avg = sum(data)/len(data)
+
+        # update list
+        data.append(new_val)
+        new_avg = sum(data)/len(data)
+
+        assert new_avg == incremental_average(avg, new_val, len(data))
